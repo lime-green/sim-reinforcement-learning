0434edfd1bcfdcc1ce1bc2799acd58ca616d3f38
afonsohfontes <afonsohfontes@gmai.com>
2023-03-07 01:58:49 +0100

    I dont recall what I changed <3

--- src/model/learn.py
+++ src/model/learn.py
@@ -37,23 +37,23 @@ def create_single_env(env_kwargs):
 def learn():
     best = 0
     j = 0
-    for i in range(40):
-        env_kwargs = dict(sim_duration=15, reward_type="delta_damage", mask_invalid_actions=True, print=False)
+    model_load_path = f"./models/best-{model.__class__.__name__}"
+    for i in range(1000):
+        env_kwargs = dict(sim_duration=20, reward_type="delta_damage", mask_invalid_actions=True, print=False)
         env = create_multi_env(4, env_kwargs)
         #env = create_single_env(env_kwargs)
         model = MaskedDQN(MaskedPolicy, env, verbose=0)
         #model = MaskablePPO("MlpPolicy", env, verbose=1)
         #model = PPO("MlpPolicy", env, verbose=1)
-
-        model_load_path = f"./models/{j}-{model.__class__.__name__}"
-        #model_save_path = f"./models/{i}-{model.__class__.__name__}"
+        if j>0:
+            model_load_path = f"./models/{j}-{model.__class__.__name__}"
         if os.path.exists(f"{model_load_path}.zip"):
             print("Loading existing model...")
             model.load(model_load_path, env=env)
             print("Done loading model")
 
-        model.learn(total_timesteps=100000, progress_bar=True)
-        current = evaluate_policy(model, model.env, n_eval_episodes=20, deterministic=True, callback=policy_callback, render=False)
+        model.learn(total_timesteps=500000, progress_bar=True)
+        current = evaluate_policy(model, model.env, n_eval_episodes=120, deterministic=True, callback=policy_callback, render=False)
         if current[1]>best:
             best = current[1]
             j = i
@@ -62,6 +62,11 @@ def learn():
             model.save(model_load_path, exclude=["policy_kwargs"])
             print("Done saving model")
 
+    print("---- Done! Best model was number ", j, " ------")
+    model_load_path = f"./models/{j}-{model.__class__.__name__}"
+    model.load(model_load_path, env=env)
+    model_load_path = f"./models/best-{model.__class__.__name__}"
+    model.save(model_load_path, exclude=["policy_kwargs"])
 
 if __name__ == "__main__":
     learn()
