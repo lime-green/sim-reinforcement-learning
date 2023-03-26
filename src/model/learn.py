3104d1acd6cd1dfc1de4cca32c5a5ddcfde29e5a
Jason <jbolla@gmail.com>
2023-03-26 11:00:08 -0700

    remove training iterations and run lint

--- src/model/learn.py
+++ src/model/learn.py
@@ -10,6 +10,7 @@ from model.dqn import MaskedDQN, MaskedPolicy
 from model.ppo import MaskablePPO
 from environment.environment import WoWSimsEnv
 
+
 def policy_callback(locals, globals_):
     if locals["info"]["is_success"]:
         print("DPS", locals["info"]["dps"], "Steps", locals["info"]["steps"])
@@ -20,6 +21,7 @@ def create_env(**kwargs):
     env = FlattenObservation(env)
     return env
 
+
 def create_multi_env(num_envs, env_kwargs):
     return make_vec_env(
         create_env,
@@ -29,13 +31,16 @@ def create_multi_env(num_envs, env_kwargs):
         env_kwargs=env_kwargs,
     )
 
+
 def create_single_env(env_kwargs):
     return create_env(**env_kwargs)
+
 
 def initialize_environment(count, env_kwargs):
     if count == 1:
         return create_single_env(env_kwargs)
     return create_multi_env(count, env_kwargs)
+
 
 def initialize_model(env, verbose):
     model_type = os.environ.get("MODEL_TYPE", "PPO")
@@ -48,11 +53,16 @@ def initialize_model(env, verbose):
     load_latest_file(model, env)
     return model
 
+
 def find_highest_file_index(modelName):
     if not os.path.exists(f"./models/{modelName}/"):
         print("no path")
         return -1
-    return max([int(f[:f.index('.')]) for f in os.listdir(f"./models/{modelName}/")], default=-1)
+    return max(
+        [int(f[: f.index(".")]) for f in os.listdir(f"./models/{modelName}/")],
+        default=-1,
+    )
+
 
 def save_file(model):
     new_file_index = find_highest_file_index(model.__class__.__name__) + 1
@@ -61,6 +71,7 @@ def save_file(model):
     model.save(model_save_path, exclude=["policy_kwargs"])
     print("Done saving model")
     return model_save_path
+
 
 def load_latest_file(model, env):
     fileIndex = find_highest_file_index(model.__class__.__name__)
@@ -73,18 +84,19 @@ def load_latest_file(model, env):
         model.load(model_load_path, env=env)
         print("Done loading model")
 
-def learn():
-    best_result = 0
-    best_file_path = None
 
+def learn():
     verbose = bool(int(os.environ.get("VERBOSE", 0)))
     environment_count = os.environ.get("ENVIRONMENT_COUNT", 16)
     episode_duration_seconds = os.environ.get("EPISODE_DURATION_SECONDS", 60)
     simulation_step_duration_msec = os.environ.get("SIMULATION_STEP_DURATION_MSEC", 50)
-    episodes_per_training_iteration = os.environ.get("EPISODES_PER_TRAINING_ITERATION", 400)
-    training_iteration_count = os.environ.get("TRAINING_ITERATION_COUNT", 60)
+    episodes_per_training_iteration = os.environ.get(
+        "EPISODES_PER_TRAINING_ITERATION", 400
+    )
     reward_type = os.environ.get("REWARD_TYPE", "delta_damage")
-    steps_per_episode = math.ceil((episode_duration_seconds * 1000) / simulation_step_duration_msec)
+    steps_per_episode = math.ceil(
+        (episode_duration_seconds * 1000) / simulation_step_duration_msec
+    )
     env_kwargs = dict(
         sim_duration_seconds=episode_duration_seconds,
         sim_step_duration_msec=simulation_step_duration_msec,
@@ -94,23 +106,20 @@ def learn():
     env = initialize_environment(environment_count, env_kwargs)
     model = initialize_model(env, verbose)
 
-    for i in range(training_iteration_count):
-        model.learn(total_timesteps=(steps_per_episode * episodes_per_training_iteration), progress_bar=True)
-        current = evaluate_policy(
-            model,
-            env,
-            n_eval_episodes=20,
-            deterministic=True,
-            callback=policy_callback,
-            render=False,
-        )
-        new_file_path = save_file(model)
-        if current[1] > best_result:
-            best_result = current[1]
-            best_file_path = new_file_path
+    model.learn(
+        total_timesteps=(steps_per_episode * episodes_per_training_iteration),
+        progress_bar=True,
+    )
+    evaluate_policy(
+        model,
+        env,
+        n_eval_episodes=20,
+        deterministic=True,
+        callback=policy_callback,
+        render=False,
+    )
+    save_file(model)
 
-    if best_file_path != None:
-        print(f"---- Done! Best model was {best_file_path}.zip ----")
 
 if __name__ == "__main__":
     learn()
